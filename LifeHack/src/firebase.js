00fa7a61c9ff49110083de0b28a45b77579c4323
kellyngkq <100149281+kellyngkq@users.noreply.github.com>
2023-05-28 13:35:03 +0800

    linked to firebase

---
+++ LifeHack/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHfFHJ7XQx66ULz54ayCkb12AFslAd5Ek",
+  authDomain: "lifehack-eight.firebaseapp.com",
+  projectId: "lifehack-eight",
+  storageBucket: "lifehack-eight.appspot.com",
+  messagingSenderId: "5040274339",
+  appId: "1:5040274339:web:59fb5639665d51454fb816",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
